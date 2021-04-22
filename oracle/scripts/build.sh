62dd5e3c9b68290b76b20c5690aa6045f160373a
Franklin Waller <fr.l.waller@gmail.com>
2021-04-22 16:39:56 +0200

    WIP: Simulation tests

--- oracle/scripts/build.sh
+++ oracle/scripts/build.sh
@@ -4,3 +4,4 @@ cd "`dirname $0`"
 source flags.sh
 cargo build --target wasm32-unknown-unknown --release
 cp ./../../target/wasm32-unknown-unknown/release/oracle.wasm ../../res
+cp ../../res/oracle.wasm ../tests/it/wasm
--- token/scripts/build.sh
+++ token/scripts/build.sh
@@ -3,4 +3,5 @@ set -e
 cd "`dirname $0`"
 source flags.sh
 cargo build --target wasm32-unknown-unknown --release
-cp ./target/wasm32-unknown-unknown/release/token.wasm ./res
+cp ./../../target/wasm32-unknown-unknown/release/token.wasm  ../../res
+cp ../../res/token.wasm ../../oracle/tests/it/wasm
