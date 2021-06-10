c20ca73f9bea8d9d4eb8b781540426ac6dd18dea
jasperdg <jasperdegooijer@gmail.com>
2021-06-10 09:40:19 +0200

    altered shell scripts to work better in monorepo environment, rebuilding all projects in the workspace on build

--- oracle/scripts/build.sh
+++ oracle/scripts/build.sh
@@ -1,7 +1,5 @@
-  #!/bin/bash
+#!/bin/bash
 set -e
-cd "`dirname $0`"
-source flags.sh
-cargo build --target wasm32-unknown-unknown --release
-cp ./../../target/wasm32-unknown-unknown/release/oracle.wasm ../../res
-cp ../../res/oracle.wasm ../tests/it/wasm
+cd ../
+bash build.sh
+cp target/wasm32-unknown-unknown/release/amm.wasm res/
