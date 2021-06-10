f0ca32de53adaeaff7976ac80a098664ff29741d
jasperdg <jasperdegooijer@gmail.com>
2021-06-10 09:44:17 +0200

    added  as type

--- oracle/src/types.rs
+++ oracle/src/types.rs
@@ -1,6 +1,6 @@
-use near_sdk::json_types::U64;
+use near_sdk::json_types::{U64, U128};
 /// Raw type for timestamp in nanoseconds
 pub type Timestamp = u64;
 pub type WrappedTimestamp = U64;
 pub type Duration = u64;
-// pub type WrappedBalance = U128;
+pub type WrappedBalance = U128;
