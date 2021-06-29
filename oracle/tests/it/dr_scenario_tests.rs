cd134b0460df7be586f0a3ec20f619c48d0a81c2
Jameson Hodge <contact@jamesonhodge.com>
2021-06-29 12:29:54 -0400

    cleanup dr_scenario_1 and print balances

--- oracle/tests/it/dr_scenario_tests.rs
+++ oracle/tests/it/dr_scenario_tests.rs
@@ -1,59 +1,71 @@
 use crate::utils::*;
 use oracle::whitelist::CustomFeeStakeArgs;
 
-// Scenario: Alice (final arbitrator) creates a data request; Bob stakes on a
-// correct outcome, and Carol stakes on an incorrect outcome. Bob and Carol
-// take turns maxing out the bond size until final arbitrator is triggered.
-// Alice finalizes, Bob and Carol collect the correct rewards.
+// Scenario: Bob and Carol take turns staking the exact bond size each round. Bob
+// ends the escalation game on a correct outcome
 #[test]
 fn dr_scenario_1() {
-
     // configure test options and create data request
+    let validity_bond = 1;
     let init_res = TestUtils::init(Some(TestSetupArgs {
         custom_fee: CustomFeeStakeArgs::None,
-        validity_bond: 1,
+        validity_bond,
         final_arbitrator_invoke_amount: 2500
     }));
-    //let init_balance_alice = init_res.alice.get_token_balance(None);
+    let init_balance_alice = init_res.alice.get_token_balance(None);
+    let init_balance_bob = init_res.bob.get_token_balance(None);
+    let init_balance_carol = init_res.carol.get_token_balance(None);
+    println!("Alice balance before data request creation: {}", init_balance_alice);
     let _new_dr_res = init_res.alice.dr_new();
-    // println!("{:?}", new_dr_res);    
     let dr_exist = init_res.alice.dr_exists(0);
     assert!(dr_exist, "something went wrong during dr creation");
-
-    for i in 2..12 {
-        println!("Round {}", i-1);
-        let stake_amount = 2u128.pow(i) * 10u128.pow(24);
-        println!("Bond size: {}", stake_amount);
+    
+    println!("Alice balance after data request creation:  {}", init_res.alice.get_token_balance(None));
+    println!("Bob balance before staking:                 {}", init_balance_bob);
+    println!("Carol balance before staking:               {}", init_balance_carol);
+    
+    for i in 0..11 {
+        let stake_amount = 2u128.pow(i+2) * 10u128.pow(24); // stake 2, 4, 16, 32, ...
         // even numbers => Bob stakes on correct outcome
         // odd numbers => Carol stakes on incorrect outcome
-        match i % 2 == 0 {
+        match (i+2) % 2 == 0 {
             true => {
+                println!("Round {}, bond size: {}, staking correctly with Bob", i, stake_amount);
                 let outcome_to_stake = data_request::Outcome::Answer(data_request::AnswerType::String("test".to_string()));
-                println!("Bob balance before staking:   {}", init_res.bob.get_token_balance(None));
                 let _res = init_res.bob.stake(0, outcome_to_stake, stake_amount);
-                println!("Bob balance after staking:    {}", init_res.bob.get_token_balance(None));
             },
             false => {
+                println!("Round {}, bond size: {}, staking incorrectly with Carol", i, stake_amount);
                 let outcome_to_stake = data_request::Outcome::Answer(data_request::AnswerType::String("test_wrong".to_string()));
-                println!("Carol balance before staking: {}", init_res.bob.get_token_balance(None));
                 let _res = init_res.carol.stake(0, outcome_to_stake, stake_amount);
-                println!("Carol balance after staking:  {}", init_res.bob.get_token_balance(None));
             }
         };
     }
-
-    // round 4
-    //let result = init_res.carol.stake(0, wrong_outcome.clone(), (validity_bond.pow(5)) * 10u128.pow(24));
-
-    //println!("{:?}", result);
-
-    // let _post_stake_balance_oracle = init_res.alice.get_token_balance(Some(ORACLE_CONTRACT_ID.to_string()));
-    // let post_stake_balance_alice = init_res.alice.get_token_balance(None);
-    //assert_eq!(post_stake_balance_alice, init_balance_alice - stake_cost - dr_cost);
+    
+    // get balances before finalization and claim
+    let pre_claim_balance_bob = init_res.bob.get_token_balance(None);
+    let pre_claim_balance_carol = init_res.carol.get_token_balance(None);
     
     init_res.alice.finalize(0);
-    // init_res.alice.claim(0);
+    init_res.bob.claim(0);
+    init_res.carol.claim(0);
     
-    //let post_claim_balance_alice = init_res.alice.get_token_balance(None);
-    //assert_eq!(post_claim_balance_alice, init_balance_alice);
+    // get balances and differences from after staking/before claiming and before staking
+    let post_balance_alice = init_res.alice.get_token_balance(None);
+    let post_balance_bob = init_res.bob.get_token_balance(None);
+    let post_balance_carol = init_res.carol.get_token_balance(None);
+    let post_stake_difference_bob = post_balance_bob - pre_claim_balance_bob;
+    let post_stake_difference_carol = post_balance_carol - pre_claim_balance_carol;
+    let post_total_difference_bob = init_balance_bob - pre_claim_balance_bob;
+    let post_total_difference_carol = init_balance_carol - pre_claim_balance_carol;
+    let post_total_difference_alice = init_balance_alice - post_balance_alice;
+
+    println!("Bob gained {} from claim for a total profit of {}", post_stake_difference_bob, post_total_difference_bob);
+    println!("Carol gained {} from claim for a total loss of {}", post_stake_difference_carol, post_total_difference_carol);
+    println!("Alice lost {} altogether", post_total_difference_alice);
+
+    println!("Alice final balance:  {}", post_balance_alice);
+    println!("Bob final balance:    {}", post_balance_bob);
+    println!("Carol final balance:  {}", post_balance_carol);
+    
 }
