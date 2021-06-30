bfa3f6c08ad2360c2fb714a78ded8232297f474c
Jameson Hodge <contact@jamesonhodge.com>
2021-06-30 10:00:45 -0400

    data request: cover more cases in assert_can_finalize

--- oracle/src/data_request.rs
+++ oracle/src/data_request.rs
@@ -470,7 +470,8 @@ impl DataRequestView for DataRequest {
 
     fn assert_can_finalize(&self) {
         assert!(!self.final_arbitrator_triggered, "Can only be finalized by final arbitrator: {}", self.request_config.final_arbitrator);
-        let last_window = self.resolution_windows.iter().last().expect("No resolution windows found, DataRequest not processed");
+        assert!(self.resolution_windows.iter().count() >= 2, "No bonded outcome found");
+        let last_window = self.resolution_windows.iter().last().unwrap();
         self.assert_not_finalized();
         assert!(env::block_timestamp() >= last_window.end_time, "Challenge period not ended");
     }
@@ -1260,7 +1261,7 @@ mod mock_token_basic_tests {
     }
 
     #[test]
-    #[should_panic(expected = "No resolution windows found, DataRequest not processed")]
+    #[should_panic(expected = "No bonded outcome found")]
     fn dr_finalize_no_resolutions() {
         testing_env!(get_context(token()));
         let whitelist = Some(vec![registry_entry(bob()), registry_entry(carol())]);
